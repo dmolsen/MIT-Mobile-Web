e1f2badac1596391ecc47fb5db8a7f4d98c7fc3c
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-07-17 15:18:11 -0400

    playing around with a config file

---
+++ config.inc.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Copyright (c) 2008 Massachusetts Institute of Technology
+ * 
+ * Licensed under the MIT License
+ * Redistributions of files must retain the above copyright notice.
+ * 
+ */
+
+/* Quick Configuration File for MIT Mobile Web OSP */
+
+$inst_name = "WVU"; # name of educational institution
+$org_name = "Web Services"; # name of organization supporting this instance
+
+?>
