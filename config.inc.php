a0d56fe157db67e0886eb7d215687ab2bc47e5bc
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-07-20 14:48:12 -0400

    updating config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -33,5 +33,4 @@ $ip_home_icon     = "wvu-logo-home.gif"; # home icon when user first pulls up yo
 $homelink_icon    = "homelink_wvu.png";  # homelink breadcrumb graphic for iPhone. should be in web/ip/images
 $non_ip_icon      = "wvu-logo.gif";      # header icon for non-iPhone phones. separate files should be in web/fp/images & web/sp/images
 
-
 ?>
