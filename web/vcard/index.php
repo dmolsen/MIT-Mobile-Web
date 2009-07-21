c7d8ce535c286df7d1f066bd22b82f25d60324d6
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-07-21 09:35:48 -0400

    vcard test

---
+++ web/vcard/index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright (c) 2008 Massachusetts Institute of Technology
+ * 
+ * Licensed under the MIT License
+ * Redistributions of files must retain the above copyright notice.
+ * 
+ */
+
+
+require "../page_builder/page_header.php";
+
+require "$prefix/index.html";
+
+$page->help_off();
+$page->output();
+
+?>
