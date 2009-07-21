0783b5d789e24dd319581432ca932011e6e4d1aa
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-07-20 22:02:12 -0400

    creating a test for capturing GPS coordinates on an iPhone & sending to GMaps

--- config.inc.php
+++ config.inc.php
@@ -17,6 +17,7 @@ $org_name         = "University Relations/Web Services";  # name of organization
 /* Misc. Copy Configuration (where "copy" means text) */
 $contact_addy      = "web_services@mail.wvu.edu"; # email address to contact w/ questions
 $mobile_web_addy   = "m.asdb-cluster.wvu.edu";    # web address for the mobile web site
+$main_site_addy    = "www.wvu.edu";               # web address for your regular web site
 $email_ending	   = "mail.wvu.edu";              # ending email addy used on directory page example
 $area_code         = "304";                       # area code for school
 $exchange          = "293";                       # next three digits in a phone number for your school
@@ -32,5 +33,8 @@ $ip_bookmark_icon = "icon_wvu.png";      # bookmark graphic for iPhone. should b
 $ip_home_icon     = "wvu-logo-home.gif"; # home icon when user first pulls up your site on iphone. should be in web/home/ip/images
 $homelink_icon    = "homelink_wvu.png";  # homelink breadcrumb graphic for iPhone. should be in web/ip/images
 $non_ip_icon      = "wvu-logo.gif";      # header icon for non-iPhone phones. separate files should be in web/fp/images & web/sp/images
+
+/* Special Features */
+$has_sms          = true;                # if you have a page showing SMS commands set to true
 
 ?>
