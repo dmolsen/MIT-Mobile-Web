1f220da932c55f7e75e385a32262a5124c996627
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-07-24 22:59:52 -0400

    seeing if i can get a list of calendars from google

--- web/calendar/day.php
+++ web/calendar/day.php
@@ -9,9 +9,17 @@
 
 
 require "../page_builder/page_header.php";
-require "../../lib/mit_calendar.php";
+#require "../../lib/mit_calendar.php";
 
-//defines all the variables related to being today
+// set-up Zend gData
+$path = '/apache/htdocs/MIT-Mobile-Web/lib/ZendGdata-1.8.4PL1/library';
+set_include_path(get_include_path() . PATH_SEPARATOR . $path);
+require_once 'Zend/Loader.php';
+Zend_Loader::loadClass('Zend_Gdata');
+Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
+Zend_Loader::loadClass('Zend_Gdata_Calendar');
+
+// defines all the variables related to being today
 require "calendar_lib.php";
 
 $time = $_REQUEST['time'];
@@ -21,8 +29,16 @@ $prev = day_info($time, -1);
 $type = $_REQUEST['type'];
 $Type = ucwords($type);
 
-$methodName = "Todays{$Type}Headers";
-$events = MIT_Calendar::$methodName($current['date']);
+#$methodName = "Todays{$Type}Headers";
+#$events = MIT_Calendar::$methodName($current['date']);
+
+$user = 'user@gmail.com';
+$pass = 'pass';
+$service = Zend_Gdata_Calendar::AUTH_SERVICE_NAME; // predefined service name for calendar
+
+$client = Zend_Gdata_ClientLogin::getHttpClient($user,$pass,$service);
+$gdataCal = new Zend_Gdata_Calendar($client);
+$calFeed = $gdataCal->getCalendarListFeed();
 
 require "$prefix/day.html";
 $page->output();
