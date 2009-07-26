ca2a6998fd73fe42453ee438770b37250ec852c4
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-07-26 14:55:58 -0400

    cleaning up the gcal test

--- web/calendar/day.php
+++ web/calendar/day.php
@@ -10,16 +10,11 @@
 
 require "../page_builder/page_header.php";
 
-//various copy includes
+// various copy includes
 require_once "../../config.gen.inc.php";
 
-// set-up Zend gData
-$path = '/apache/htdocs/MIT-Mobile-Web/lib/ZendGdata-1.8.4PL1/library';
-set_include_path(get_include_path() . PATH_SEPARATOR . $path);
-require_once 'Zend/Loader.php';
-Zend_Loader::loadClass('Zend_Gdata');
-Zend_Loader::loadClass('Zend_Gdata_ClientLogin');
-Zend_Loader::loadClass('Zend_Gdata_Calendar');
+// sets up google calendar classes
+require "gcalendar_setup.php";
 
 // defines all the variables related to being today
 require "calendar_lib.php";
@@ -34,19 +29,16 @@ $Type = ucwords($type);
 #$methodName = "Todays{$Type}Headers";
 #$events = MIT_Calendar::$methodName($current['date']);
 
-$user = 'user@gmail.com';
-$pass = 'pass';
 $service = Zend_Gdata_Calendar::AUTH_SERVICE_NAME; // predefined service name for calendar
 
-$client = Zend_Gdata_ClientLogin::getHttpClient($user,$pass,$service);
+$client = Zend_Gdata_ClientLogin::getHttpClient($username.'@gmail.com',$password,$service);
 $gdataCal = new Zend_Gdata_Calendar($client);
 $query = $gdataCal->newEventQuery();
 $query->setUser('e5c02e64sdtcq4vqtt8441ib50fovr3f%40import.calendar.google.com');
 $query->setVisibility('public');
 $query->setProjection('basic');
-#$query->setQuery('yom');
-#$query->setOrderby('starttime');
-#$query->setFutureevents('true');
+$query->setOrderby('starttime');
+$query->setFutureevents('true');
 $eventFeed = $gdataCal->getCalendarEventFeed($query);
 
 require "$prefix/day.html";
