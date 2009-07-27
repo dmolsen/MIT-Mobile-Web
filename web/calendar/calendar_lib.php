4b4660a616e05d4d51bacaefa462da73c2d2918a
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-07-27 09:48:20 -0400

    adding support for google's calendar date format

--- web/calendar/calendar_lib.php
+++ web/calendar/calendar_lib.php
@@ -20,6 +20,7 @@ function day_info($time, $offset=0) {
     "day_3Let"      => date('D', $time),
     "day_num_2dig"  => date('d', $time),
     "date"          => date('Y/m/d', $time),
+    "gdate"         => date('Y-m-d', $time),
     "time"          => strtotime(date("Y-m-d 12:00:00", $time))
   );
 }
