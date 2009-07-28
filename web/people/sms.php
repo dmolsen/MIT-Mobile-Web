5ad7967d8d646ccb80c1a3c05775e4c5733d1de4
Dave Olsen <dave@asdb-1.systems.wvu.edu>
2009-07-28 16:50:18 -0400

    initial tweaks to directory SMS

--- web/people/sms.php
+++ web/people/sms.php
@@ -30,7 +30,7 @@ if (isset($_REQUEST["username"])) {
    $people = html_escape_people($people);
 
    $total = count($people);
-
+   
    if($total==0) {
      $failed_search = True;
      require "$prefix/sms/index.html";
@@ -46,7 +46,7 @@ if (isset($_REQUEST["username"])) {
    require "$prefix/sms/index.html";
 }
 
-$page->output();
+#$page->output();
 
 function detail_url($person) {
     return $_SERVER['SCRIPT_NAME'] . '?username=' . urlencode($person["id"]) . '&filter=' . urlencode($_REQUEST['filter']);
