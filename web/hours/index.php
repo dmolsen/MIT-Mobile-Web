b8b36e12518ba1ba8417a6a32da0c926b9cf9362
Dave Olsen <dave@asdb-1.systems.wvu.edu>
2009-07-29 13:17:36 -0400

    copy and PHP syntax changes to get hours working

--- web/hours/index.php
+++ web/hours/index.php
@@ -18,15 +18,14 @@ $hours_locations['cl_ec_evansdale'] = array("evansdale","Evansdale Computing Lab
 $hours_locations['cl_dc_whitehall'] = array("erc","ERC Computing Lab","Evansdale","The ERC Computing Lab is in the basement of Bennett Tower in the Evansdale Residential Complex (ERC), also known as “Towers.” ","2pm-12am","8am-12am","8am-12am","8am-12am","8am-12am","8am-6pm","10am-6pm");
 
 $days = array("Sun","Mon","Tue","Wed","Thu","Fri","Sat");
+$i = 0;
 
 // dynamic pages need to include dynamics scripts
 switch($_REQUEST['page']) {
-
-  $i = 0;
   
   // overall computer labs cases
   case "cl":
-    require "$phone/cl/index.html"
+    require "$prefix/cl/index.html";
     $page->cache();
     $page->output();
     break;
@@ -57,6 +56,10 @@ function clecevansdaleURL() {
 
 function clecercURL() {
   return "./?page=cl_ec_erc";
+}
+
+function clURL() {
+  return "./?page=cl";
 }
 
 ?>
