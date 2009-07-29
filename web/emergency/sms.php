8a513fec85f714349dfa93f4da62ec1e591453e8
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-07-28 23:01:32 -0400

    adding emergency sms handler

---
+++ web/emergency/sms.php
@@ -0,0 +1,33 @@
+<?
+/**
+ * Copyright (c) 2008 Massachusetts Institute of Technology
+ * 
+ * Licensed under the MIT License
+ * Redistributions of files must retain the above copyright notice.
+ * 
+ */
+
+require_once "../../lib/rss_services.php";
+
+$emergencies = $Emergency->get_feed();
+
+if($emergencies === False) {
+  $paragraphs = array('Emergency information is currently not available');
+} else {
+  foreach ($emergencies as $title => $emergency) {
+	$text = explode("\n", $emergency[$title]['text']);
+	$paragraphs = array();
+	foreach($text as $paragraph) {
+	  if($paragraph) {
+	    echo($paragraph);
+	  }
+	}
+  }
+
+  // handle the case that an emergency RSS feed doesn't return data until emergency (like e2campus)
+  if ($paragraphs == False) {
+	echo("There is currently no emergency on campus.");
+  }
+}
+
+?>
