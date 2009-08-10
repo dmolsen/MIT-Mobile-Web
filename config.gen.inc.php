df835c41f98e07e73cc768609878be4a74fae188
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-08-10 08:09:09 -0400

    adding a new test calendar for extended properties

--- config.gen.inc.php
+++ config.gen.inc.php
@@ -53,6 +53,7 @@ $calendars['academic'] = array('title' => 'Academics', 'user' => 'd259da0o994a5o
 $calendars['athletics'] = array('title' => 'Athletics', 'user' => 'rnle9udl9r9kl00qb1r1o7pb6oj57f34@import.calendar.google.com');
 $calendars['lectures'] = array('title' => 'Lectures &amp; Speakers', 'user' => 'nkl93kssu9e2evgc9o5vfkssgq0v1rqs@import.calendar.google.com');
 $calendars['special_events'] = array('title' => 'Special Events', 'user' => 'h3nc9ba8tslh4ovpu69rk206rpc9eg01@import.calendar.google.com');
+$calendars['ext_special_events'] = array('title' => 'Extended Special Events', 'user' => 'bip4uahi9dla3k9rbgl8pltd9dtd20v6@import.calendar.google.com');
 
 /* News Info - uses first entry as default entry */
 $news_srcs = array();
