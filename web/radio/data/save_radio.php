41ab0f6c87f21e1d6c0340ba0eb39a078e1ba703
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-08-16 11:08:44 -0400

    initial load of the u92 section

---
+++ web/radio/data/save_radio.php
@@ -0,0 +1,40 @@
+<?
+
+require_once "../../../lib/db.php";
+require_once "radio.import.inc.php";
+
+$days = array("Sun","Mon","Tue","Wed","Thu","Fri","Sat");
+									
+$db = db::$connection;
+$stmt_1 = $db->prepare("INSERT INTO RadioShows (name) values (?)");	
+						
+foreach($shows as $show) {
+	if (db::$use_sqlite) {
+		$stmt_1->execute(array($show[0]));
+	}
+	else {
+		$stmt_1->bind_param('s', $show[0]);
+		$stmt_1->execute();
+	}
+}
+
+$stmt_2 = $db->prepare("INSERT INTO RadioShowTimes (start,end,day,show) values (?,?,?,?)");	
+						
+foreach($show_times as $show_time) {
+	$start = $show_time[0];
+	$end = $show_time[1];
+	$i = 0;
+	foreach($show_time[2] as $show) {
+		if (db::$use_sqlite) {
+			$stmt_2->execute(array($start,$end,$days[$i],$show));
+		}
+		else {
+			$stmt_2->bind_param('iisi', $start,$end,$days[$i],$show));
+			$stmt_2->execute();
+		}
+		$i++;
+	}
+	
+}
+	
+?>
---
+++ web/radio/help.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Copyright (c) 2008 Massachusetts Institute of Technology
+ * 
+ * Licensed under the MIT License
+ * Redistributions of files must retain the above copyright notice.
+ * 
+ */
+
+require_once "../../config.gen.inc.php";
+
+$header = $inst_name." Useful Links";
+$module = "links";
+
+$help = array(
+  'Get quick access to mobile-optimized websites of organizations providing affiliated services to the MIT community.',
+);
+
+require "../page_builder/help.php";
+
+?>
