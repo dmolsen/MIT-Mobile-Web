8fd241043fedae4bf6273a71f53bbbb05651a5ea
Dave Olsen <dave@asdb-1.systems.wvu.edu>
2009-08-23 10:21:45 -0400

    various layout changes for youtoube

--- web/youtube/lib/youtube.lib.inc.php
+++ web/youtube/lib/youtube.lib.inc.php
@@ -14,15 +14,15 @@ set_include_path(get_include_path() . PATH_SEPARATOR . $path);
 require_once 'Zend/Loader.php';
 Zend_Loader::loadClass('Zend_Gdata_YouTube');
 
-function printVideoFeed($videoFeed) {
+function printVideoFeed($videoFeed,$phone) {
   if ($phone == 'ip') { echo("<ul class='results'>"); }
   foreach ($videoFeed as $videoEntry) {
-    printVideoEntry($videoEntry);
+    printVideoEntry($videoEntry,$phone);
   }
   if ($phone == 'ip') { echo("</ul>"); }
 }
 
-function printVideoEntry($videoEntry) {
+function printVideoEntry($videoEntry,$phone) {
 	
   $videoThumbnails = $videoEntry->getVideoThumbnails();
 
@@ -31,19 +31,19 @@ function printVideoEntry($videoEntry) {
   $secs = $seconds % 60;
   
   $updated = getdate(strtotime($videoEntry->getUpdated()->text)); 
-  
+
   if ($phone == 'ip') {
 	 echo("<li>");
   } else {
 	 echo("<p class='focal' style='height: 62px'>");
   }
   
-  echo("<img src='".$videoThumbnails[1]['url']."' hspace=6 height=60 width=80 align=left alt='YouTube Video Thumbnail'><a href='".$videoEntry->getVideoWatchPageUrl()."'>".$videoEntry->getVideoTitle()."</a><span class='smallprint'><br />Duration: ".$mins.":".$secs."<br />Updated: ".$updated['month']." ".$updated['mday'].", ".$updated['year']);
+  echo("<a href='".$videoEntry->getVideoWatchPageUrl()."'><img src='".$videoThumbnails[1]['url']."' hspace=6 height=60 width=80 align=left alt='YouTube Video Thumbnail'>".$videoEntry->getVideoTitle()."<span class='smallprint'><br />Duration: ".$mins.":".$secs."<br />Updated: ".$updated['month']." ".$updated['mday'].", ".$updated['year']."</a>");
 
   if ($phone == 'ip') {
-    echo("</li>")
+    echo("</li>");
   } else {
-    echo("</p>")
+    echo("</p>");
   }
   
 }
