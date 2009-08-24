283e01a10b86d73e1bd6102d645182db343ac995
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-08-24 08:46:48 -0400

    fixing the link for the DA on the useful links page

--- web/links/index.php
+++ web/links/index.php
@@ -17,7 +17,7 @@ class Links {
     'eCampus' => 'ecampus.wvu.edu',
     'MIX' => 'www.mix.wvu.edu',
     'SOLE' => 'sole.hsc.wvu.edu',
-    'Daily Athenaeum' => 'www.da.wvu.edu',
+    'Daily Athenaeum' => 'www.thedaonline.com',
     'MSNsportsNET.com' => 'mobile.msnsportsnet.com'
   );
 }
