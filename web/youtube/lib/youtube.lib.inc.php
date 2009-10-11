b22e100369cef458cdefb398e8683af7b4855243
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-10-11 12:21:29 -0400

    forgot to take out a useless var from GA code

--- web/youtube/lib/youtube.lib.inc.php
+++ web/youtube/lib/youtube.lib.inc.php
@@ -39,7 +39,7 @@ function printVideoEntry($videoEntry,$phone) {
   }
   
   $outgoing = urlencode($videoEntry->getVideoTitle());
-  echo("<a href='".$videoEntry->getVideoWatchPageUrl()."' class='youtube' onClick='javascript: pageTracker._trackPageview(\"/outgoing/youtube/<?=$key?>/<?=$outgoing?>\");'><img src='".$videoThumbnails[1]['url']."' hspace=6 height=60 width=80 align=left alt='YouTube Video Thumbnail'>".$videoEntry->getVideoTitle());
+  echo("<a href='".$videoEntry->getVideoWatchPageUrl()."' class='youtube' onClick='javascript: pageTracker._trackPageview(\"/outgoing/youtube/<?=$outgoing?>\");'><img src='".$videoThumbnails[1]['url']."' hspace=6 height=60 width=80 align=left alt='YouTube Video Thumbnail'>".$videoEntry->getVideoTitle());
 
   if ($phone == 'ip')
     echo("<span class='smallprint'><br />".$mins.":".$secs." mins. | ".substr($updated['month'],0,3)." ".$updated['mday']." ".$updated['year']."</a>");
