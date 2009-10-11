e6779ffe82918b1adb1a7bb522ed15881a9a19a4
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-10-11 12:21:56 -0400

    added a data dir for managing the links. also added GA outgoing code

---
+++ web/links/data/data.inc.php
@@ -0,0 +1,14 @@
+<?
+
+class Links {
+  public static $links = array(
+    'GroupWise' => 'wvugw.wvu.edu',
+    'eCampus' => 'ecampus.wvu.edu',
+    'MIX' => 'www.mix.wvu.edu',
+    'SOLE' => 'sole.hsc.wvu.edu',
+    'Daily Athenaeum' => 'www.thedaonline.com',
+    'MSNsportsNET.com' => 'mobile.msnsportsnet.com'
+  );
+}
+
+?>
--- web/links/index.php
+++ web/links/index.php
@@ -7,20 +7,9 @@
  * 
  */
 
-
 require_once "../page_builder/page_header.php";
 require_once "../../config.gen.inc.php";
-
-class Links {
-  public static $links = array(
-    'GroupWise' => 'wvugw.wvu.edu',
-    'eCampus' => 'ecampus.wvu.edu',
-    'MIX' => 'www.mix.wvu.edu',
-    'SOLE' => 'sole.hsc.wvu.edu',
-    'Daily Athenaeum' => 'www.thedaonline.com',
-    'MSNsportsNET.com' => 'mobile.msnsportsnet.com'
-  );
-}
+require_once "data/data.inc.php";
 
 $links = array();
 foreach(Links::$links as $name => $link) {
