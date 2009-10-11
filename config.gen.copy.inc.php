bec0117dffb82bd79bd2cc4be3bd5b8194008d0a
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-10-11 11:23:15 -0400

    removed references to WVU in the base config set-up

--- config.gen.copy.inc.php
+++ config.gen.copy.inc.php
@@ -8,17 +8,18 @@
  * 
  */
 
-/* Quick Configuration File for MIT Mobile Web OSP */
+/* Quick Configuration File for Mobile Web */
 
 /* Institution & Org Info */
-$inst_name         = "WVU";                                # name of educational institution. highly recommend an acronym.
-$org_name          = "University Relations/Web Services";  # name of organization supporting this instance
+$inst_name         = "HEU";                       # name of educational institution. highly recommend an acronym.
+$inst_name_full    = "Higher Ed University";      # full name of education institution
+$org_name          = "Higher Ed Tech Office";     # name of organization supporting this instance
 
 /* Misc. Copy Configuration (where "copy" means text) */
-$contact_addy      = "web_services@mail.wvu.edu"; # email address to contact w/ questions
-$mobile_web_addy   = "m.asdb-cluster.wvu.edu";    # web address for the mobile web site
-$main_site_addy    = "www.wvu.edu";               # web address for your regular web site
-$email_ending	   = "mail.wvu.edu";              # ending email addy used on directory page example
+$contact_addy      = "mobile@mail.inst.edu";      # email address to contact w/ questions
+$mobile_web_addy   = "m.inst.edu";                # web address for the mobile web site
+$main_site_addy    = "www.inst.edu";              # web address for your regular web site
+$email_ending	   = "mail.inst.edu";             # ending email addy used on directory page example
 $area_code         = "304";                       # area code for school
 $exchange          = "293";                       # next three digits in a phone number for your school
 $has_oc_prefix     = true;                        # does your school have a shorter, on-campus prefix?
@@ -29,13 +30,7 @@ $dir_search_samp_l = "3042930000";                # long phone number example fo
 $dir_search_samp_s = "30000";                     # short phone number example for directory search, leave empty if you don't want to use it
 
 /* Shared Graphics Info */
-$ip_bookmark_icon  = "icon_wvu.png";      # bookmark graphic for iPhone. should be in web/ip/images
-$ip_home_icon      = "wvu-logo-home.gif"; # home icon when user first pulls up your site on iphone. should be in web/home/ip/images
-$homelink_icon     = "homelink_wvu.png";  # homelink breadcrumb graphic for iPhone. should be in web/ip/images
-$non_ip_icon       = "wvu-logo.gif";      # header icon for non-iPhone phones. separate files should be in web/fp/images & web/sp/images
-
-/* Special Features */
-$has_sms           = true;               # if you have a page showing SMS commands set to true
-
+$ip_bookmark_icon  = "icon.png";      # bookmark graphic for iPhone. should be in web/ip/images
+$homelink_icon     = "homelink.png";  # homelink breadcrumb graphic for iPhone. should be in web/ip/images
 
 ?>
