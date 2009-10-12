48780ea85977f475d13de4aa3e753d9285846ce1
David Olsen <dmolsen@david-olsens-macbook-pro.local>
2009-10-12 15:23:24 -0400

    fixing the save buildings script

--- web/map/data/save_buildings_file.php
+++ web/map/data/save_buildings_file.php
@@ -3,7 +3,7 @@
 require_once "../../../lib/db.php";
 											
 $db = db::$connection;
-$stmt = $db->prepare("INSERT INTO Buildings (name,latitude,longitude,physical_address,type,subtype,code,parent,phone,website,hours,campus) values (?, ?, ?, ?, ?, ?,?,?,?,?,?,?,?)");
+$stmt = $db->prepare("INSERT INTO Buildings (name,latitude,longitude,physical_address,type,subtype,code,parent,wifi,phone,website,hours,campus) values (?,?,?,?,?,?,?,?,?,?,?,?,?)");
 
 $filename = "buildings.txt";
 $fd = fopen ($filename, "r");
@@ -14,10 +14,10 @@ $lines = explode("\r", $contents);
 foreach ($lines as $line) {
 	$fields = explode("\t", $line);
 	if (db::$use_sqlite) {
-		$stmt->execute(array($fields[1], $fields[2], $fields[3], $fields[4], $fields[5], $fields[6], $fields[7], $fields[8], $fields[9], $fields[10], $fields[11],$fields[12], $fields[13]));
+		$stmt->execute(array($fields[0], $fields[1], $fields[2], $fields[3], $fields[4], $fields[5], $fields[6], $fields[7], $fields[8], $fields[9], $fields[10],$fields[11], $fields[12]));
 	}
 	else {
-		$stmt->bind_param('ssssssssssss', $fields[1], $fields[2], $fields[3], $fields[4], $fields[5], $fields[6], $fields[7], $fields[8], $fields[9], $fields[10], $fields[11],$fields[12], $fields[13]);
+		$stmt->bind_param('ssssssssssss', $fields[0], $fields[1], $fields[2], $fields[3], $fields[4], $fields[5], $fields[6], $fields[7], $fields[8], $fields[9], $fields[10], $fields[11],$fields[12]);
 		$stmt->execute();
 	}
 }
