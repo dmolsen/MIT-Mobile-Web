a6d3d118a9428a7256ddf57596e04a32e9f2a4c8
David Olsen <dmolsen@david-olsens-macbook-pro-3.local>
2010-12-01 14:51:15 -0500

    the create SQL script for the MySQL database. must have a database named mobilewebosp.

---
+++ db/create.sql
@@ -0,0 +1,34 @@
+CREATE TABLE `mobilewebosp`.`Buildings1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `code` varchar(255) DEFAULT NULL,
+  `parent` int(11) DEFAULT NULL,
+  `hours` varchar(255) DEFAULT NULL,
+  `website` varchar(255) DEFAULT NULL,
+  `wifi` varchar(45) DEFAULT NULL,
+  `phone` varchar(45) DEFAULT NULL,
+  `subtype` varchar(45) DEFAULT NULL,
+  `campus` varchar(45) DEFAULT NULL,
+  `latitude` varchar(45) DEFAULT NULL,
+  `longitude` varchar(45) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `physical_address` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `mobilewebosp`.`Pageviews_by_Device1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `count` int(11) DEFAULT NULL,
+  `device` varchar(255) DEFAULT NULL,
+  `day` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `mobilewebosp`.`Pageviews_by_Section1` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `count` int(11) DEFAULT NULL,
+  `section` varchar(255) DEFAULT NULL,
+  `day` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+
