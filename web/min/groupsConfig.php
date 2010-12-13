ece72383f9b61679228434c2b11b2024581d1091
David Olsen <dmolsen@david-olsens-macbook-pro-3.local>
2010-12-13 09:36:20 -0500

    modified the minifier groups config to let it dynamically add module javascript init files

--- web/min/groupsConfig.php
+++ web/min/groupsConfig.php
@@ -11,7 +11,23 @@
 
 require("../../config.gen.inc.php");
 
+$js_a = array('//templates/webkit/javascripts/jqtouch/jqtouch.js', '//templates/webkit/javascripts/db.init.js', '//templates/webkit/javascripts/ga.init.js', '//templates/webkit/javascripts/aria.init.js');
+$css_a = array('//templates/webkit/javascripts/jqtouch/jqtouch.css', '//themes/'.$theme.'/webkit/stylesheets/theme.css', '//themes/'.$theme.'/webkit/stylesheets/extra.css');
+
+// dynamically load module init files so there's less configuration
+$base_dir = getcwd();
+$base_dir = str_replace("min","",$base_dir);
+$files = scandir($base_dir);
+foreach ($files as $file) {
+	if (is_dir($base_dir.$file)) {
+		$js_file = $base_dir.$file."/templates/webkit/javascripts/".$file.".init.js";
+		if (file_exists($js_file)) {
+			$js_a[] = "//".$file."/templates/webkit/javascripts/".$file.".init.js";
+		}
+	}
+}
+
 return array(
-	'js' => array('//templates/webkit/javascripts/jqtouch/jqtouch.js', '//templates/webkit/javascripts/db.init.js', '//templates/webkit/javascripts/ga.init.js', '//templates/webkit/javascripts/aria.init.js', '//calendar/templates/webkit/javascripts/calendar.init.js', '//people/templates/webkit/javascripts/people.init.js', '//map/templates/webkit/javascripts/map.init.js'),
-    'css' => array('//templates/webkit/javascripts/jqtouch/jqtouch.css', '//themes/'.$theme.'/webkit/stylesheets/theme.css', '//themes/'.$theme.'/webkit/stylesheets/extra.css'),
+	'js' => $js_a,
+    'css' => $css_a
 );
