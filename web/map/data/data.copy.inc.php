54ce6b652e44f5d1ecc4a472542fd22a279a3f8c
David Olsen <dmolsen@david-olsens-macbook-pro-3.local>
2010-12-28 12:07:18 -0500

    removing the basic markers array since they're not needed anymore. slimmed down the category listing

--- web/map/data/data.copy.inc.php
+++ web/map/data/data.copy.inc.php
@@ -21,36 +21,24 @@ $mobilemap = false; 		# on static maps use mobile version of tiles
  * cat-key         => array("text for a view title",
  * 							"text for a view title",
  *							"text for a view title",
- *							true or false is it a searchable type
  */
 class Categorys {
   public static $info = array(
-    "names"        => array("Building name", "Building Names", "Buildings by Name", "Building"),
-	"dining"       => array("Dining location", "Dining Location Names", "Dining Locations", "Dining"),
-    "wifi"         => array("WiFi-enabled location", "WiFi-enabled Locations", "WiFi-enabled Locations", "WiFi-enabled Location")
+    "names"        => array("Building name", "Building Names", "Buildings by Name"),
+	"dining"       => array("Dining location", "Dining Location Names", "Dining Locations"),
+	"codes"        => array("Building code", "Building Codes", "Buildings by Code"),
+    "wifi"         => array("WiFi-enabled location", "WiFi-enabled Locations", "WiFi-enabled Locations")
   );
 }
 
-# Smartphone and feature phone specific marker types. key has to be a type and not a subtype
-# these images can be found in web/themes/[theme_name]/basic/images/markers THOUGH...
-# the options below are passed to Google Static Maps and drawn. The images in
-# 'markers' are copies for including outside of the map. so if you change
-# the graphics below make sure you grab a copy for 'markers'
-function marker_type($type) {
-	$markers = array(
-	    "Building" 				 => "midredb",
-	    "Dining" 				 => "midpurpled"
-	);
-	return $markers[$type];	
-}
-
-# iPhone specific marker types. the key can be either the type or subtype for the building
+# Map markers based on the types from the database. The key can be either the type or subtype for the building
 # these images can be found in web/themes/[theme_name]/webkit/images/markers
 # they were pulled from: http://code.google.com/p/google-maps-icons/
 $marker_types = array();
 $marker_types['Building'] 				= 'apartment';
 $marker_types['Dining'] 				= 'restaurant';
 $marker_types['Art'] 					= 'theater';
+
 
 
 ?>
