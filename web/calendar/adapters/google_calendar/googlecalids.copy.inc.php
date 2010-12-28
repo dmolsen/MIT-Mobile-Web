2515db988dc6b71c7e2256e2d1fc6d8ca4608088
David Olsen <dmolsen@david-olsens-macbook-pro-3.local>
2010-12-28 11:55:01 -0500

    separating the google calendar IDs array into it's own file for easier editing

---
+++ web/calendar/adapters/google_calendar/googlecalids.copy.inc.php
@@ -0,0 +1,8 @@
+<?
+
+	// the keys for this array should match the keys in the $calendars array from data.inc.php
+	// IMPORTANT: their should be an 'all' category that contains all events for search purposes
+	$google_cal_ids = array("all" => "pcnqpk03212bhbvvq03hc0ouv8k0oj76@import.calendar.google.com",
+						    "academic" => "7q0f77k0oi0uiet6el58htdptdi1f4r5@import.calendar.google.com");
+
+?>
