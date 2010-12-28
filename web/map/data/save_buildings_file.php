8fcf48072114b38b4820747c3879bd946440cc12
David Olsen <dmolsen@david-olsens-macbook-pro-3.local>
2010-12-28 12:00:54 -0500

    adding support for the new UID field for import

--- web/map/data/save_buildings_file.php
+++ web/map/data/save_buildings_file.php
@@ -23,8 +23,8 @@ foreach ($lines as $line) {
 	$fields = explode("\t", $line);
 	echo("\nImporting '".$fields[0]."'...");
 	$types = array('text','text','text','text','text','text','text','integer','text','text','text','text','text');
-	$stmt =& $db->connection->prepare("INSERT INTO Buildings (name,latitude,longitude,physical_address,type,subtype,code,parent,wifi,phone,website,campus,hours) values (?,?,?,?,?,?,?,?,?,?,?,?,?)",$types);
-	$stmt->execute(array($fields[0], $fields[1], $fields[2], $fields[3], $fields[4], $fields[5], $fields[6], $fields[7], $fields[8], $fields[9], $fields[10],$fields[11],$fields[12]));		
+	$stmt =& $db->connection->prepare("INSERT INTO Buildings (name,latitude,longitude,physical_address,type,subtype,code,parent,wifi,phone,website,hours,campus,uid) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?)",$types);
+	$stmt->execute(array($fields[0], $fields[1], $fields[2], $fields[3], $fields[4], $fields[5], $fields[6], $fields[7], $fields[8], $fields[9], $fields[10],$fields[11],$fields[12],$fields[13]));		
 }
 echo("\nCompleted importing building data...\n");
 
