4b8001f386a5922e5cf6c0807ab86848ea1bdb66
David Olsen <dmolsen@david-olsens-macbook-pro-3.local>
2011-01-02 20:51:28 -0500

    adding support for defining which specific adapter should be called

--- web/page_builder/adapter.php
+++ web/page_builder/adapter.php
@@ -17,10 +17,14 @@ require_once "../../lib/spyc-0.4.5/spyc.php";
 
 class ModuleAdapter {  
 	
-	// returns the adapter for the requesting module
-	public static function find() {
+	// returns the adapter for the requesting module or, if $dir is supplied, the adapter for that particular module
+	public static function find($dir = nil) {
+		$extra = "";
+		if ($dir != nil) {
+			$extra = "/../".$dir; // just offering a way to jump to another modules info.yml file
+		}
 		$base_dir = getcwd();
-		$config_file = $base_dir."/info.yml";
+		$config_file = $base_dir.$extra."/info.yml";
 		if (file_exists($config_file)) {
 			$config = Spyc::YAMLLoad($config_file);
 			return $config['adapter'];
